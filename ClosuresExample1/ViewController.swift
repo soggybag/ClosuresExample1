d2fc0fa95b663c4ccbd665a769aa9440fc5ea3e1
Mitchell Hudson <mitchellhudson@mitchells-mbp.gateway.sonic.net>
2016-08-24 11:36:08 -0700

    init commit
    A sample swift project illustrating a practical use of closures

--- ClosuresExample1/ViewController.swift
+++ ClosuresExample1/ViewController.swift
@@ -6,9 +6,47 @@
 //  Copyright © 2016 Mitchell Hudson. All rights reserved.
 //
 
+
+// This example shows closures used in a typical UIKit app
+//
+// Exercise: 
+// 1) Find all of closures
+// 2) 
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        let alert = UIAlertController(title: "Login", message: "", preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "OK", style: .Default) { (action) in
+            self.doLogin()
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            self.loginCanceled()
+        }
+        
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Username"
+        }
+        
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Password"
+            textField.secureTextEntry = true
+        }
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        presentViewController(alert, animated: true, completion: fadeBack)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +57,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func doLogin() {
+        print("Doing log in stuff...")
+        fadeUp()
+    }
 
-
+    func loginCanceled() {
+        print("You canceled login")
+        fadeUp()
+    }
+    
+    func fadeBack() {
+        UIView.animateWithDuration(1) { 
+            self.view.backgroundColor = UIColor.blackColor()
+        }
+    }
+    
+    func fadeUp() {
+        UIView.animateWithDuration(1) {
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    func fadeRed() {
+        UIView.animateWithDuration(1) { 
+            self.view.backgroundColor = UIColor.redColor()
+        }
+    }
 }
 
